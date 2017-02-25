d31cc7a666f588cad63482c36a771199c5cb3968
Michal Bryc <furchin@gmail.com>
2017-02-25 17:07:09 -0500

    Initial complexifier algorithm

--- index.js
+++ index.js
@@ -3,14 +3,33 @@ const port = process.env.PORT || 8080
 
 const restify = require('restify');
 
-const respond = (request, response, next) => {
-  response.send('hello ' + request.params.name)
+const transform = (request, response, next) => {
+  var s = request.params.s;
+  // Step 1: Measure existing complexity.
+  // For a first pass, let's just use string length.
+  const initialComplexity = s.length;
+
+  // Step 2: Do stuff.
+  // Some ideas:
+  // 1. Use a thesaurus
+  // 2. Translate to and back from another language
+  // 3. Map of common words and phrases to more complex phrases.
+  // 4. negated opposite (eg change "has" to "doesn't not have")
+  // For a first pass, let's map using maps/phrases.json
+  const phraseMap = require('./maps/phrases.json');
+  for (var phrase in phraseMap) {
+    s = s.replace(phrase, phraseMap[phrase]);
+  }
+
+  // Step 3: Ensure complexity has been increased and return value.
+  const finalComplexity = s.length;
+
+  response.send((initialComplexity < finalComplexity) ? s : request.params.s);
   next()
 }
 
 const server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+server.get('/transform/:s', transform);
 
 server.get(/\//, restify.serveStatic({
   directory: './static',
